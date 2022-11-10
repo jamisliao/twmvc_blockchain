051b3009acfe258191bc934010eea410ff06f71e
Jimmy <jiaming0708@gmail.com>
2022-11-10 18:21:22 +0800

    check login status

--- twmvc_example/twmcv_example.Client/Pages/Index.razor.cs
+++ twmvc_example/twmcv_example.Client/Pages/Index.razor.cs
@@ -9,6 +9,8 @@ public partial class Index
   [Inject] private HttpClient _http { get; set; }
   private string Url;
   private string AuthenticationId;
+  private string Address;
+  private Timer checkAuthTimer;
 
   protected override async Task OnInitializedAsync()
   {
@@ -19,5 +21,21 @@ public partial class Index
     AuthenticationId = result.outPut.Result["AuthenticationId"];
     
     StateHasChanged();
+    
+    checkAuthTimer = new Timer(CheckAuth, null, 0, 1000);
+  }
+
+  private async void CheckAuth(object? state)
+  {
+    var result = await _http.GetFromJsonAsync<APIResult>($"api/flow/login/result?authenticationId={AuthenticationId}");
+    if (result.outPut.ErrorCode != 0)
+    {
+      return;
+    }
+
+    await checkAuthTimer.DisposeAsync();
+    Url = "";
+    Address = result.outPut.Result["Address"];
+    await InvokeAsync(StateHasChanged);
   }
 }
