f0278d174cef69fd1418058fb45cef74da7b1d02
Jimmy <jiaming0708@gmail.com>
2022-11-11 10:47:01 +0800

    implement transfer amount

--- twmvc_example/twmcv_example.Client/Pages/Index.razor.cs
+++ twmvc_example/twmcv_example.Client/Pages/Index.razor.cs
@@ -10,7 +10,13 @@ public partial class Index
   private string Url;
   private string AuthenticationId;
   private string Address;
-  private Timer checkAuthTimer;
+  private Timer checkTimer;
+  private string ReceiveAddress;
+  private int TransferAmount;
+  private string TransactionAuthId;
+  private string TransactionSessionId;
+  private string TransactionId;
+  private string TransactionResultUrl;
 
   protected override async Task OnInitializedAsync()
   {
@@ -22,20 +28,76 @@ public partial class Index
     
     StateHasChanged();
     
-    checkAuthTimer = new Timer(CheckAuth, null, 0, 1000);
+    checkTimer = new Timer(CheckAuth, null, 0, 1000);
   }
 
   private async void CheckAuth(object? state)
   {
-    var result = await _http.GetFromJsonAsync<APIResult>($"api/flow/login/result?authenticationId={AuthenticationId}");
-    if (result.outPut.ErrorCode != 0)
+    try
     {
-      return;
-    }
+      var result =
+        await _http.GetFromJsonAsync<APIResult>($"api/flow/login/result?authenticationId={AuthenticationId}");
+      if (result.outPut.ErrorCode != 0)
+      {
+        return;
+      }
 
-    await checkAuthTimer.DisposeAsync();
-    Url = "";
-    Address = result.outPut.Result["Address"];
-    await InvokeAsync(StateHasChanged);
+      await checkTimer.DisposeAsync();
+      Url = "";
+      Address = result.outPut.Result["Address"];
+      await InvokeAsync(StateHasChanged);
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine(e.ToString());
+    }
+  }
+
+  private async void Mutate()
+  {
+    try
+    {
+      TransactionResultUrl = "";
+      Url = "";
+      
+      var response =
+        await _http.PostAsync($"api/flow/transaction/{Address}/to/{ReceiveAddress}/{TransferAmount}", null);
+      var result = await response.Content.ReadFromJsonAsync<APIResult>();
+      Url = result.outPut.Result["Url"];
+      TransactionAuthId = result.outPut.Result["AuthorizationId"];
+      TransactionSessionId = result.outPut.Result["SessionId"];
+      
+      StateHasChanged();
+
+      checkTimer = new Timer(CheckTransaction, null, 0, 1000);
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine(e.ToString());
+    }
+  }
+
+  private async void CheckTransaction(object? state)
+  {
+    try
+    {
+      var response =
+        await _http.PostAsync($"api/flow/transaction/{TransactionAuthId}/{TransactionSessionId}", null);
+      var result = await response.Content.ReadFromJsonAsync<APIResult>();
+      if (result.outPut.ErrorCode != 0)
+      {
+        return;
+      }
+
+      await checkTimer.DisposeAsync();
+      Url = "";
+      TransactionId = result.outPut.Result["txId"];
+      TransactionResultUrl = result.outPut.Result["FlowScanUrl"];
+      await InvokeAsync(StateHasChanged);
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine(e.ToString());
+    }
   }
 }
